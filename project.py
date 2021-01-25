e0572cd0c9c36e67059e97f738128a9a496acc5e
boris1592 <53943159+boris1592@users.noreply.github.com>
2021-01-25 11:09:57 +0300

    Update project.py

--- project.py
+++ project.py
@@ -129,6 +129,7 @@ def convert(inpt, name, minfreq, maxfreq, pxs, wavrate):
     tms = timeit.default_timer()
 
     progress_label.configure(text=f"Преобразование в процессе, осталось: 100%")
+    root.destroy()
     print("Преобразование в процессе, осталось: 100%")
     print("Успешно завершено за %d секунд." % int(tms-tm))
 
